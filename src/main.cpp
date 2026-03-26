606c891754f77ab3ac41c7aba715c4f7c3cd10c5
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-03-26 09:53:27 +0700

    feat: input path prompt

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,35 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 using namespace std;
 
 int main() {
-    std::ifstream in("input.txt");
-    std::ofstream out("output.txt");
+    std::filesystem::path inputPath = "test/input.txt";
+    std::cout << "Input file path: ";
+    std::cin >> inputPath;
+    std::ifstream in(inputPath);
+
+    std::filesystem::path outputPath = "test/tokenized-" + inputPath.filename().string();
+    std::ofstream out(outputPath);
 
     std::stringstream buffer;
     buffer << in.rdbuf();
 
     arion::Tokenizer tokenizer(buffer.str());
+    tokenizer.setDebug(true);
 
     while (true) {
         arion::Token t = tokenizer.getNextToken();
 
         if (t.type == tokenizer.TOKEN_EOF) break;
 
-        std::string result = tokenizer.tokenToString(t, tokenizer.getLexeme());
+        std::string result = tokenizer.tokenToString(t);
 
         if (!result.empty()) {
             out << result << "\n";
         }
     }
+
+    std::cout << "Outputted to " << outputPath << std::endl;
 }
