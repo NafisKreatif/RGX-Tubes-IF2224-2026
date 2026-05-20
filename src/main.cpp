8122aa51f0cd141a3f763bde4c0c7f14cb44a233
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-05-20 11:52:40 +0700

    feat: main for milestone 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "arion/ASTBuilder.hpp"
+#include "arion/DecoratedAST.hpp"
 #include "arion/Parser.hpp"
 #include "arion/Tokenizer.hpp"
 #include <filesystem>
@@ -17,7 +18,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    std::filesystem::path outputPath = "test/milestone-2/ast-" + inputPath.filename().string();
+    std::filesystem::path outputPath = "test/milestone-3/decorated-ast-" + inputPath.filename().string();
     std::ofstream out(outputPath);
 
     arion::Tokenizer tokenizer;
@@ -33,9 +34,17 @@ int main(int argc, char **argv) {
         arion::ASTBuilder builder;
 
         arion::ASTNode astTree = builder.build(parseResult);
-
         astTree.printTree(out);
+
+        arion::DecoratedAST decoratedAstTree(astTree);
+
+        decoratedAstTree.printTable(out);
+        decoratedAstTree.printTree(out);
+
     } catch (const arion::ParserError &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
