d0c71fe43641bac315981debdd5d26cf5c35cab5
An-Dafa <andafaanzaa@gmail.com>
2026-05-28 21:39:32 +0700

    feat: add indirect memory access instructions
    Extend the stack-machine intermediate code with LDA, IXA, LDI, and STI
    instructions to support address calculation and dynamic array access.

--- src/arion/IntermediateCode.cpp
+++ src/arion/IntermediateCode.cpp
@@ -24,6 +24,14 @@ std::string InstructionLine::opCodeToString(InstructionOpCode opCode) {
             return "JPC";
         case InstructionOpCode::OPR:
             return "OPR";
+        case InstructionOpCode::LDA:
+            return "LDA";
+        case InstructionOpCode::IXA:
+            return "IXA";
+        case InstructionOpCode::LDI:
+            return "LDI";
+        case InstructionOpCode::STI:
+            return "STI";
         case InstructionOpCode::RET:
             return "RET";
     }
@@ -37,7 +45,10 @@ int InstructionLine::operationToInt(OperationCode operation) {
 std::string InstructionLine::toString(std::size_t lineNumber) const {
     std::stringstream ss;
     ss << lineNumber << ' ' << opCodeToString(opCode);
-    if (opCode != InstructionOpCode::RET || !operand.empty()) {
+    if (!operand.empty() ||
+        (opCode != InstructionOpCode::RET &&
+         opCode != InstructionOpCode::LDI &&
+         opCode != InstructionOpCode::STI)) {
         ss << ' ' << level << ' ' << operand;
     }
 
@@ -83,15 +94,27 @@ std::size_t IntermediateCode::emitLIT(std::string value, std::string comment) {
 }
 
 std::size_t IntermediateCode::emitLOD(int address, std::string comment) {
-    return emitInstruction(InstructionOpCode::LOD, 0, std::to_string(address), std::move(comment));
+    return emitLOD(0, address, std::move(comment));
+}
+
+std::size_t IntermediateCode::emitLOD(int level, int address, std::string comment) {
+    return emitInstruction(InstructionOpCode::LOD, level, std::to_string(address), std::move(comment));
 }
 
 std::size_t IntermediateCode::emitSTO(int address, std::string comment) {
-    return emitInstruction(InstructionOpCode::STO, 0, std::to_string(address), std::move(comment));
+    return emitSTO(0, address, std::move(comment));
+}
+
+std::size_t IntermediateCode::emitSTO(int level, int address, std::string comment) {
+    return emitInstruction(InstructionOpCode::STO, level, std::to_string(address), std::move(comment));
 }
 
 std::size_t IntermediateCode::emitCAL(int line, std::string comment) {
-    return emitInstruction(InstructionOpCode::CAL, 0, std::to_string(line), std::move(comment));
+    return emitCAL(0, line, std::move(comment));
+}
+
+std::size_t IntermediateCode::emitCAL(int level, int line, std::string comment) {
+    return emitInstruction(InstructionOpCode::CAL, level, std::to_string(line), std::move(comment));
 }
 
 std::size_t IntermediateCode::emitINT(int size, std::string comment) {
@@ -108,6 +131,25 @@ std::size_t IntermediateCode::emitJPC(int line, std::string comment) {
 
 std::size_t IntermediateCode::emitOPR(OperationCode operation, std::string comment) {
     return emitInstruction(InstructionOpCode::OPR, 0, std::to_string(InstructionLine::operationToInt(operation)), std::move(comment));
+}
+
+std::size_t IntermediateCode::emitLDA(int level, int address, std::string comment) {
+    return emitInstruction(InstructionOpCode::LDA, level, std::to_string(address), std::move(comment));
+}
+
+std::size_t IntermediateCode::emitIXA(int low, int high, int elementSize, std::string comment) {
+    return emitInstruction(InstructionOpCode::IXA, low,
+                           std::to_string(high) + " " +
+                           std::to_string(elementSize),
+                           std::move(comment));
+}
+
+std::size_t IntermediateCode::emitLDI(std::string comment) {
+    return emitInstruction(InstructionOpCode::LDI, 0, "", std::move(comment));
+}
+
+std::size_t IntermediateCode::emitSTI(std::string comment) {
+    return emitInstruction(InstructionOpCode::STI, 0, "", std::move(comment));
 }
 
 std::size_t IntermediateCode::emitRET(std::string comment) {
